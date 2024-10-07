3e1440eda82ab8542104c362eefe13bbb1db8ad7
shanmugabaskar <baskar.shan@gmail.com>
2024-10-07 10:02:54 +0530

    Add project files.

---
+++ DelegatePrintAppl/PrintString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatePrintAppl
+{
+    internal class PrintString
+    {
+        static FileStream fs;
+        static StreamWriter sw;
+
+        // delegate declaration
+        public delegate void printString(string s);
+
+        // this method prints to the console
+        public static void WriteToScreen(string str)
+        {
+            Console.WriteLine("The String is: {0}", str);
+        }
+
+        //this method prints to a file
+        public static void WriteToFile(string s)
+        {
+            fs = new FileStream("d:\\message.txt",
+            FileMode.Append, FileAccess.Write);
+            sw = new StreamWriter(fs);
+            sw.WriteLine(s);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+        // this method takes the delegate as parameter and uses it to
+        // call the methods as required
+        public static void sendString(printString ps)
+        {
+            ps("Hello World");
+        }
+    }
+}
---
+++ DelegatePrintAppl/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static DelegatePrintAppl.PrintString;
+
+printString ps1 = new printString(WriteToScreen);
+printString ps2 = new printString(WriteToFile);
+sendString(ps1);
+sendString(ps2);
+Console.ReadKey();
